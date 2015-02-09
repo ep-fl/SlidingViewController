3f521f661f9acb476e039882da3e233025d75a69
ep_fl <socialinfo.my@gmail.com>
2015-02-09 16:43:00 +0300

    Initial Commit

---
+++ SlidingViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidingViewController
+//
+//  Created by fl on 09.02.15.
+//  Copyright (c) 2015 qwerty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
